fe61c419d7d4d1c063532c6ddd1c783505372eb9
Colby Ackerman <csaz86@umsystem.edu>
2019-09-16 07:23:23 -0500

    Added File.h, File.c

---
+++ Files.h
@@ -0,0 +1,18 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -/*
+Author - - - Colby Ackerman - - - - - - - - - - - - - - - - - - - - - - - - - -
+Class  - - - Operating Systems (CS4760) - - - - - - - - - - - - - - - - - - - -
+Date   - - - Sep. 14, 2019  - - - - - - - - - - - - - - - - - - - - - - - - - -
+File   - - - "Files.h"  - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+#ifndef FILES_H
+#define FILES_H
+
+#include <stdio.h>
+
+extern FILE* inFile;
+extern FILE* outFile;
+
+
+
+#endif
